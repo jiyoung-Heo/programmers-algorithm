264fae3f01ea1dd93c7fb73f276a22ff35a45579
jiyoung-Heo <jiyoungHeo.dev@gmail.com>
2021-12-03 16:53:25 +0900

    improve : dp이용 연습문제 level2 가장큰정사각형찾기

--- src/연습문제/level2/가장큰정사각형찾기/Main.java
+++ src/연습문제/level2/가장큰정사각형찾기/Main.java
@@ -3,42 +3,33 @@ package 연습문제.level2.가장큰정사각형찾기;
 //dp 문제 라는데 모르게씀,,
 
 public class Main {
-    public static void main(String[] args) {
-        Main main = new Main();
-        int[][] board = {{0,1,1,1}, {1,1,1,1}, {1,1,1,1}, {0,0,1,0}};
-//        int[][] board = {{0,0,1,1},{1,1,1,1}};
-        System.out.println(main.solution(board));
-    }
-    public int solution(int[][] board) {
-        int last = 0;
-        if (board[0].length > board.length) {
-            last = board.length-1;
-        } else {
-            last = board[0].length-1;
-        }
-        int answer = 0;
+	public static void main(String[] args) {
+		Main main = new Main();
+		int[][] board = {{0, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {0, 0, 1, 0}};
+		//        int[][] board = {{0,0,1,1},{1,1,1,1}};
+		System.out.println(main.solution(board));
+	}
 
-        for (int i = 0; i < board.length; i++) {
-            int cnt = 1;
-            boolean check = true;
-            for (int j = 0; j < board[0].length; j++) {
-                if(i+cnt <= last && j+cnt <= last){
-                    for (int k = 0; k <= cnt; k++) {
-                        for (int kk = 0; kk <= cnt; kk++) {
-                            if (board[i + k][j + kk] == 0) {
-                                check = false;
-                                break;
-                            }
-                        }
+	public int solution(int[][] board) {
+		int[][] arr = new int[board.length + 1][board[0].length + 1];
+		for (int i = 0; i < board.length; i++) {
+			for (int k = 0; k < board[0].length; k++) {
+				arr[i + 1][k + 1] = board[i][k];
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int k = 0; k < arr[0].length; k++) {
+                if(arr[i][k] != 0){
+                    arr[i][k] = Math.min(arr[i-1][k],Math.min(arr[i][k-1],arr[i-1][k-1]))+1;
+                    if(arr[i][k] >= max){
+                        max = arr[i][k];
                     }
                 }
             }
-            System.out.println(cnt);
-            if (check) {
-                answer = (cnt + 1) * (cnt + 1);
-                cnt++;
-            }
         }
-        return answer;
+
+        return max*max;
     }
 }
