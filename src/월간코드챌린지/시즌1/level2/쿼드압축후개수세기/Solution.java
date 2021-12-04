9dd55794e59bea722322c5c6ee4be6377d33099b
jiyoung-Heo <jiyoungHeo.dev@gmail.com>
2021-12-04 19:43:48 +0900

    improve : 분할정복(DFS)이용 월간코드챌린지시즌1 level2 쿼드압축후개수세기
    분할하고 재귀돌리기. 이런문제많이풀어봐야할것같다.

--- src/월간코드챌린지/시즌1/level2/쿼드압축후개수세기/Solution.java
+++ src/월간코드챌린지/시즌1/level2/쿼드압축후개수세기/Solution.java
@@ -2,12 +2,43 @@ package 월간코드챌린지.시즌1.level2.쿼드압축후개수세기;
 
 //7시 16분시작
 //재귀문제인데,, 재귀너무어려움 어떻게해야할지 잘 모르겠음.
+//분할정복, dfs 사용하기!
+
 public class Solution {
+	int[] answer;
+
 	public int[] solution(int[][] arr) {
-		int[] answer = {};
-		boolean[][] visited = new boolean[arr.length][arr.length];
-
-
+		answer = new int[2];
+		quad(arr.length, 0, 0, arr);
 		return answer;
 	}
+
+	//n : 변의 길이
+	//x,y : 시작지점의 위치
+	public void quad(int n, int y, int x, int[][] arr) {
+		if (n == 1) {
+			answer[arr[y][x]]++;
+			return;
+		}
+		if (isBlock(n, y, x, arr)) {
+			return;
+		}
+		quad(n / 2, y, x, arr);
+		quad(n / 2, y + n / 2, x, arr);
+		quad(n / 2, y, x + n / 2, arr);
+		quad(n / 2, y + n / 2, x + n / 2, arr);
+	}
+
+	public boolean isBlock(int n, int y, int x, int[][] arr) {
+		for (int i = y; i < y + n; i++) {
+			for (int k = x; k < x + n; k++) {
+				if (arr[i][k] != arr[y][x]) {
+					return false;
+				}
+			}
+		}
+		answer[arr[y][x]]++;
+		return true;
+	}
+
 }
