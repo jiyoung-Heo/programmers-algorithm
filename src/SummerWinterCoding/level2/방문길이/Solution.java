2672640cb7ac852ec73595c530f43857ed258561
Nylah <jiyoungHeo.dev@gmail.com>
2021-12-27 17:21:50 +0900

    improve: summer/winterCording level2 방문길이
    방향성을 가지지 않아서 실패했었는데
    방향성을 가지도록 4개의배열을 설정하고 출발지[][] 도착지[][] 입력하는식으로 변경함

--- src/SummerWinterCoding/level2/방문길이/Solution.java
+++ src/SummerWinterCoding/level2/방문길이/Solution.java
@@ -5,46 +5,62 @@ package SummerWinterCoding.level2.방문길이;
 //방향성을 가져야 하는데 그렇게 구현하지 못했다
 //25점맞음
 public class Solution {
-	public static void main(String[] args) {
-		Solution s = new Solution();
-		System.out.println(s.solution("UDU"));
-	}
-	public int solution(String dirs) {
-		int answer = 0;
-		int[][] arr = new int[11][11];
-		int x = 5;
-		int y = 5;
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("LULLLLLLU"));
+    }
 
-		String[] dirArr = dirs.split("");
-		for (String s : dirArr) {
-			if (arr[x][y] == 0) {
-				System.out.println(x+","+y+"방문함");
-				arr[x][y] = 1;
-				answer++;
-			}
-			switch (s) {
-				case "U":
-					if (y > 0) {
-						y -= 1;
-					}
-					break;
-				case "D":
-					if (y < 10) {
-						y += 1;
-					}
-					break;
-				case "L":
-					if (x > 0) {
-						x -= 1;
-					}
-					break;
-				case "R":
-					if (x < 10) {
-						x += 1;
-					}
-					break;
-			}
-		}
-		return answer;
-	}
+    public int solution(String dirs) {
+        int answer = 0;
+        //왼 오 위 아래
+        boolean[][][][] visited = new boolean[11][11][11][11];
+
+        int x = 5;
+        int y = 5;
+
+        String[] dirArr = dirs.split("");
+        for (String s : dirArr) {
+            int moveX = x;
+            int moveY = y;
+            switch (s) {
+                case "U":
+                    if (y < 10) {
+                        moveY++;
+                        answer = answerPlus(visited,x,y,moveX,moveY,answer);
+                        y = moveY;
+                    }
+                    break;
+                case "D":
+                    if (y > 0) {
+                        moveY--;
+                        answer = answerPlus(visited,x,y,moveX,moveY,answer);
+                        y = moveY;
+                    }
+                    break;
+                case "L":
+                    if (x > 0) {
+                        moveX--;
+                        answer = answerPlus(visited,x,y,moveX,moveY,answer);
+                        x = moveX;
+                    }
+                    break;
+                case "R":
+                    if (x < 10) {
+                        moveX++;
+                        answer = answerPlus(visited,x,y,moveX,moveY,answer);
+                        x = moveX;
+                    }
+                    break;
+            }
+        }
+        return answer;
+    }
+    public int answerPlus(boolean[][][][] visited,int x,int y,int moveX,int moveY, int answer){
+        if(!visited[x][y][moveX][moveY] && !visited[moveX][moveY][x][y]){
+            visited[x][y][moveX][moveY] = true;
+            visited[moveX][moveY][x][y] = true;
+            answer++;
+        }
+        return answer;
+    }
 }
