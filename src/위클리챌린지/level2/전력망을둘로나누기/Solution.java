78dfaea95210af897eeadb3d4dd8ac2e0e1e5030
Nylah <jiyoungHeo.dev@gmail.com>
2022-01-09 00:11:21 +0900

    improve: 위클리챌린지 level2 전력망을둘로나누기
    재귀 메서드 없애고 while문으로 돌림

--- src/위클리챌린지/level2/전력망을둘로나누기/Solution.java
+++ src/위클리챌린지/level2/전력망을둘로나누기/Solution.java
@@ -1,43 +1,48 @@
 package 위클리챌린지.level2.전력망을둘로나누기;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 //3:09
 //4:13 15점
 //4:39 23점
-//반례 못찾겠다
+//풀이완료!
 public class Solution {
-	Set<Integer> right = new HashSet<>();
-
 	public int solution(int n, int[][] wires) {
-		int min = wires.length;
+		Set<Integer> right = new HashSet<>();
+		Set<Integer> left = new HashSet<>();
+
+		int min = n;
 
 		for (int i = 0; i < wires.length; i++) {
 			right.clear();
-			List<int[]> wiresList = new ArrayList<>();
-			for (int k = 0; k < wires.length; k++) {
-				if (i == k) {
-					continue;
-				}
-				int[] one = new int[2];
-				one[0] = wires[k][0];
-				one[1] = wires[k][1];
+			left.clear();
+			right.add(wires[i][0]);
+			left.add(wires[i][1]);
 
-				wiresList.add(one);
+			while(n != (right.size()+ left.size())){
+
+				for (int k = 0; k < wires.length; k++) {
+					if(i==k){
+						continue;
+					}
+					if(right.contains(wires[k][0])||right.contains(wires[k][1])){
+						right.add(wires[k][0]);
+						right.add(wires[k][1]);
+					}
+					if(left.contains(wires[k][0])||left.contains(wires[k][1])){
+						left.add(wires[k][0]);
+						left.add(wires[k][1]);
+					}
+				}
+
 			}
-			right.add(wiresList.get(0)[0]);
-			right.add(wiresList.get(0)[1]);
-			connect(wiresList);
 
 			int temp = 0;
-			int leftSize = n - right.size();
-			if (right.size() > leftSize) {
-				temp = right.size() - leftSize;
+			if (right.size() > left.size()) {
+				temp = right.size() - left.size();
 			} else {
-				temp = leftSize - right.size();
+				temp = left.size() - right.size();
 			}
 
 			if (min > temp) {
@@ -46,21 +51,5 @@ public class Solution {
 		}
 
 		return min;
-	}
-
-	public void connect(List<int[]> wires) {
-		for (int i = 1; i < wires.size(); i++) {
-			if (right.contains(wires.get(i)[0]) || right.contains(wires.get(i)[1])) {
-				right.add(wires.get(i)[0]);
-				right.add(wires.get(i)[1]);
-			}
-		}
-		for (int i = 1; i < wires.size(); i++) {
-			if (right.contains(wires.get(i)[0]) || right.contains(wires.get(i)[1])) {
-				right.add(wires.get(i)[0]);
-				right.add(wires.get(i)[1]);
-			}
-		}
-
 	}
 }
