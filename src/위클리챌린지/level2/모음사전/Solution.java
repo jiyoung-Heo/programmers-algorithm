3e49669166414e73cda176b7dfe3c1ac05ec5d5b
Nylah <jiyoungHeo.dev@gmail.com>
2022-01-09 00:12:40 +0900

    improve: 규칙발견 위클리챌린지 level2 모음사전

--- src/위클리챌린지/level2/모음사전/Solution.java
+++ src/위클리챌린지/level2/모음사전/Solution.java
@@ -1,7 +1,22 @@
 package 위클리챌린지.level2.모음사전;
+
 //2:30
 //3:05
 //35m
-//규칙 못찾아서 시도도 못해봄
+//규칙 찾으면 되는 문제였음. 근데 비슷한거 나오면 못풀듯
 public class Solution {
+	public int solution(String word) {
+		int answer = word.length();
+
+		String str = "AEIOU";
+		int[] x = {781, 156, 31, 6, 1};
+
+		for (int i = 0; i < word.length(); i++) {
+			int idx = str.indexOf(word.charAt(i));
+			answer += x[i] * idx;
+			System.out.println(answer);
+		}
+
+		return answer;
+	}
 }
